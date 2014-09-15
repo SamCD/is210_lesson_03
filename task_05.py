8023e530a2f8c0388b99da14bcca3dc9c968bae6
Sam Cohen-DeVries <samuel.cohen-devries@spsmail.cuny.edu>
2014-09-15 08:54:58 -0400

    Final changes to tasks 4 and 5

--- task_05.py
+++ task_05.py
@@ -7,31 +7,31 @@ DURATION = int(raw_input("For how many years is this loan being borrowed?"))
 PQ = raw_input("Are you pre-qualified for this loan (y/Yes/No/n)?").lower()[:1]
 import decimal
 if PRINCIPAL < 200000 and DURATION <= 15 and PQ == "y":
-    IR = decimal.Decimal(3.63)
+    IR = 363
 elif PRINCIPAL < 200000 and DURATION <= 15 and PQ == "n":
-    IR = decimal.Decimal(4.65)
+    IR = 465
 elif PRINCIPAL < 200000 and (DURATION > 15 and DURATION <= 20) and PQ == "y":
-    IR = decimal.Decimal(4.04)
+    IR = 404
 elif PRINCIPAL < 200000 and (DURATION > 15 and DURATION <= 20) and PQ == "n":
-    IR = decimal.Decimal(4.98)
+    IR = 498
 elif PRINCIPAL < 200000 and DURATION > 20 and PQ == "y":
-    IR = decimal.Decimal(5.77)
+    IR = 577
 elif PRINCIPAL < 200000 and DURATION > 20 and PQ == "n":
-    IR = decimal.Decimal(6.39)
+    IR = 639
 elif (PRINCIPAL >= 200000 and PRINCIPAL < 1000000) and DURATION <= 15 and PQ == "y":
-    IR = decimal.Decimal(3.02)
+    IR = 302
 elif (PRINCIPAL >= 200000 and PRINCIPAL < 1000000) and DURATION <= 15 and PQ == "n":
-    IR = decimal.Decimal(3.98)
+    IR = 398
 elif (PRINCIPAL >= 200000 and PRINCIPAL < 1000000) and (DURATION > 15 and DURATION <= 20) and PQ == "y":
-    IR = decimal.Decimal(3.27)
+    IR = 327
 elif (PRINCIPAL >= 200000 and PRINCIPAL < 1000000) and (DURATION > 15 and DURATION <= 20) and PQ == "n":
-    IR = decimal.Decimal(4.08)
+    IR = 408
 elif (PRINCIPAL >= 200000 and PRINCIPAL < 1000000) and (DURATION > 20) and PQ == "y":
-    IR = decimal.Decimal(4.66)
+    IR = 466
 elif PRINCIPAL <= 1000000 and DURATION <= 15 and PQ == "y":
-    IR = decimal.Decimal(2.05)
+    IR = 205
 else:
-    IR = decimal.Decimal(2.62)
-TOTAL = int(PRINCIPAL * ((1 + ((IR/100) / 12)) ** (12 * DURATION)))
+    IR = 262
+TOTAL = int(PRINCIPAL * ((1 + (IR / 10000) / 12)) ** (12 * DURATION))
 REPORT = " Loan Report for: {0} \n---------------------------------------- \n	Principal: ${1}\n	Duration: {2}\n	Pre-qualified?: {3}\n \n	Total: ${4:8}".format(NAME, PRINCIPAL, DURATION, PQ, TOTAL)
 print REPORT
